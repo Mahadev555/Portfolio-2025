49d2f02b2cb947701e89c4ba3272d1da23a2718d
= <=>
2025-09-15 10:17:35 +0530

    Added Allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['mahadev-ai.onrender.com'],
+    host: '0.0.0.0'
+  }
 })
